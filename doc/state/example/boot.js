ab4fc03b42cfa3732425e0a24891069a8bc41545
alyhxn <hassanali8812@gmail.com>
2025-08-21 16:07:12 -0600

    Using alyhxn/datatshell

--- doc/state/example/boot.js
+++ doc/state/example/boot.js
@@ -1,5 +1,6 @@
 const env = { version: 'latest' }
 const arg = { x: 321, y: 543 }
-const url = 'https://playproject.io/datashell/shim.js'
+const url = 'https://raw.githubusercontent.com/alyhxn/datashell/refs/heads/main/shim.js'
 const src = `${url}?${new URLSearchParams(env)}#${new URLSearchParams(arg)}`
+console.log(this.open ? `Loading ${src}` : `Importing ${src}`)
 this.open ? document.body.append(Object.assign(document.createElement('script'), { src })) : importScripts(src)
